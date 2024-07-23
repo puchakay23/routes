84207056617ffe85765d46136da61dafa206b6b0
puchakay23 <puchakay23@gmail.com>
2024-07-23 13:24:19 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Labs from './components/Labs';
+import Support from './components/Support';
+import Notfound from './components/Notfound';
+import { NavLink } from 'react-router-dom';
+import Mainheader from './components/Mainheader'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/support">Support</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/labs">labs</NavLink></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Mainheader/>}>
+          <Route index element={<Home/>}></Route>
+          <Route path='/support' element={<Support/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/labs' element={<Labs/>}></Route>
+          <Route path='*' element={<Notfound/>}></Route>
+        </Route>
+        
+      </Routes>
     </div>
   );
 }
