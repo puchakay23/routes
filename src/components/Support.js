defd46ad267df35d9703c1ff9bdb3493cb2be1b1
puchakay23 <puchakay23@gmail.com>
2024-07-23 13:25:50 +0530

    g

---
+++ src/components/Support.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Support = () => {
+  const navigate = useNavigate();
+  function clickHandler(){
+      navigate("/Labs");
+  }
+  function backHandler(){
+    navigate(-1);
+  }
+  return (
+    <div>
+      <div>Support</div>
+      <button onClick={clickHandler}>Move to Labs page</button><br/>
+      <button onClick={backHandler}>Go back</button>
+    </div>
+  )
+}
+
+export default Support
